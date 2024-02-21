397a89269036c0af8454162bbc4a0f199cded900
ALPOP <159054026+DEVALPOP@users.noreply.github.com>
2024-02-21 22:01:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/modymesourceze/ZeMusic",
+    "https://github.com/DEVALPOP/MZ",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
